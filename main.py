0fbaa9fcba093fb4ec1155eb423822f9f1060524
VadimVthvPro <vvadimvthvv@icloud.com>
2024-09-07 01:06:28 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import telebot
 from telebot import *
-#from aiogram import *
-#from aiogram.types import *
-#from ChatGPT import *
-#import sql
-#import threading
+from openai import OpenAI
 
 
 
-token = '7342509477:AAHKGhqdN0ZXbmEnAYLcR7uuhhSy4sB7dd8'
+client = OpenAI(api_key='sk-t5nR13z0ohiejlTjO1q1OPWt2HHLXInYD3ZwLRHsg4T3BlbkFJ7qr8cr7vaPqRY8m7WSpmOJRc3IXfi6Po3qYTLOXcMA')
+
+
+
+token = '7239777184:AAEJCJcJx4_c9fuj9pIFKT3RlofrcnVV9Nk'
 
 bot = telebot.TeleBot(token)
 
@@ -51,7 +51,7 @@ def choise_of_sex(message):
   bot.register_next_step_handler(ma, choise_of_mass)
 def choise_of_mass(message):
   global mass
-  mass = int(message.text)
+  mass = float(message.text)
   mess_before_imt = bot.send_message(message.chat.id, text = 'Начинаю вычисление вашего индекса массы тела...')
   imt = round(mass / ((length/100) ** 2), 3)
   imt_using_words = str()
@@ -64,6 +64,7 @@ def choise_of_mass(message):
   if round(imt) > 26:
     imt_using_words = 'Ожирение'
   global cal
+  cal = 0
   if sex == 'man':
     cal = (10 * mass) + (6.25 * length) - (5 * age) + 5
   if sex == 'woman':
@@ -73,20 +74,43 @@ def choise_of_mass(message):
   btn2 = 'Удержание массы'
   btn3 = 'Набор массы'
   markup.add(btn1, btn2, btn3)
-  mes = bot.send_message(message.chat.id, text = '{}, твой вес: {}, твой рост: {}, твой индекс массы тела:{}, и твой вес - это {}. Сечас ты сможешь выбрать свою цель, чтобы я смог помочь тебе с твоим персональным планом питания:'.format(message.from_user.first_name, mass, length, imt, imt_using_words), reply_markup= markup)
-  bot.register_next_step_handler(mes, quest)
+  po = bot.send_message(message.chat.id, text = '{}, твой вес: {}, твой рост: {}, твой индекс массы тела:{}, и твой вес - это {}. Сечас ты сможешь выбрать свою цель, чтобы я смог помочь тебе с твоим персональным планом питания:'.format(message.from_user.first_name, mass, length, imt, imt_using_words), reply_markup= markup)
+  bot.register_next_step_handler(po, pohelan)
 
-
-
-@bot.message_handler(content_types=['text'])
-def quest(message):
-  global ans
-  ans = message.text
-  if (ans == 'Сброс веса'):
-    bot.send_message(message.chat.id, text = 'Ваши Б/Ж/У должны быть в соотношении 35/15/50, и в день вы должны потреблять {} килокалорий'.format(cal - cal/5))
-  if (ans == 'Удержание веса'):
-    bot.send_message(message.chat.id, text = 'Ваши Б/Ж/У должны быть в соотношении 30/20-25/50-55, и в день вы должны потреблять {} килокалорий'.format(cal))
-  if (ans == 'Набор массы'):
-    bot.send_message(message.chat.id, text = 'Ваши Б/Ж/У должны быть в соотношении 30/20-25/50-55, и в день вы должны потреблять {} килокалорий'.format(cal + 450))
+def pohelan(message):
+  markup1 = types.ReplyKeyboardMarkup(resize_keyboard=True)
+  btn1 = 'Подсказка по рецепту'
+  btn2 = 'Помощь с треней'
+  markup1.add(btn1, btn2)
+  if message.text == 'Сброс веса':
+    bot.send_message(message.chat.id, text = 'Ваши Б/Ж/У должны быть в соотношении 35/15/50, и в день вы должны потреблять {} килокалорий'.format(cal - cal/5), reply_markup= telebot.types.ReplyKeyboardRemove())
+  if message.text == 'Удержание массы':
+    bot.send_message(message.chat.id, text = 'Ваши Б/Ж/У должны быть в соотношении 30/20-25/50-55, и в день вы должны потреблять {} килокалорий'.format(cal), reply_markup= telebot.types.ReplyKeyboardRemove())
+  if message.text == 'Набор массы':
+    bot.send_message(message.chat.id, text = 'Ваши Б/Ж/У должны быть в соотношении 30/20-25/50-55, и в день вы должны потреблять {} килокалорий'.format(cal + 450), reply_markup= telebot.types.ReplyKeyboardRemove())
+  markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+  btn1 = 'Подсказка по рецепту'
+  btn2 = 'Помощь с треней'
+  markup.add(btn1, btn2)
+  ans = bot.send_message(message.chat.id, text = 'Для того, чтобы тебе осуществить {}, тебе стоит наладить твоё питание и тренировки.  Этот мегабот тебе с этим поможет))'.format(message.text), reply_markup = markup)
+  bot.register_next_step_handler(ans, recept)
+def recept(message):
+  if message.text == 'Подсказка по рецепту' :
+    ans = bot.send_message(message.chat.id, text = 'Напиши ингридидиенты, которые ты хочешь использовать:',reply_markup= telebot.types.ReplyKeyboardRemove() )
+    bot.register_next_step_handler(ans, gpt)
+def gpt(message):
+    global products
+    products = message.text
+    completion = client.chat.completions.create(
+    model = 'gpt-4o',                          
+    messages = [
+        {"role": "system", "content" : "You are a nutrition assistant"},      
+        {'role': 'user', 'content': 'Придумай мне кулинарный рецепт еды из {} и выведи итоговые Б/Ж/У продукта'.format(products)}
+    ],
+    temperature = 0.5                   
+    )
+    
+    english_text = completion.choices[0].message.content
+    bot.send_message(message.chat.id, text = english_text)
 
 bot.polling(none_stop=True)
